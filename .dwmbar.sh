b5e212eb51f500af7edd826e188e2a40608d8ac7
Karandeep <kgrewal2@asu.edu>
2020-06-18 18:08:35 -0700

    :package: Adding bashrc, dwmbar, fehbg, xinitrc, dwm and st. Updating vimrc

---
+++ .dwmbar.sh
@@ -0,0 +1,21 @@
+SEP=" | "
+
+printBattery(){
+    capacity=$(cat /sys/class/power_supply/BAT0/capacity)
+    if [[ $capacity -le 10 ]]; then
+        dunstify --urgency=critical "Low Battery"
+    fi
+    if [[ $capacity -ge 90 ]]; then
+        dunstify --urgency=critical "Battery Full"
+    fi
+    echo "$capacity%"
+}
+
+printDate(){
+    printf "$(date "+%A %d %h $SEP %I:%M %p")"
+}
+
+while :; do
+    xsetroot -name "$(printBattery) $SEP $(printDate)";
+    sleep 1m
+done
